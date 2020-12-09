0ce277def26a356ba1880cca9342c0d150b80dba
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-12-08 22:56:17 -0600

    increase and decrease

--- views/FormularioPlatillo.js
+++ views/FormularioPlatillo.js
@@ -1,14 +1,82 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState, useContext} from 'react';
+import {StyleSheet, View, Image} from 'react-native';
+import {
+  Container,
+  Content,
+  Form,
+  Icon,
+  Input,
+  Grid,
+  Col,
+  Button,
+  Text,
+} from 'native-base';
+import globalStyles from '../styles/global';
+//Context
+import PedidoContext from '../context/pedidos/pedidosContext';
+import gobalStyles from '../styles/global';
+//Navigation
+import {useNavigation} from '@react-navigation/native';
 
 const FormularioPlatillo = () => {
-    return (
-        <View>
-            <Text>FormularioPlatillo</Text>
-        </View>
-    )
-}
 
-export default FormularioPlatillo
+    const [cantidad, setCantidad] = useState(1);
 
-const styles = StyleSheet.create({})
+    const calcularCantidad = (cantidad) => {
+        setCantidad(cantidad);
+    }
+
+    const disminuirUno = () => {
+        if(cantidad>0){
+            setCantidad(parseInt(cantidad)-1);
+        } else if(!cantidad){
+            setCantidad(1);
+        }
+    }
+
+    const incremetarUno = () => {
+        if(!cantidad){
+            setCantidad(1);
+        }else{
+            setCantidad(parseInt(cantidad)+1);
+        }
+    }
+
+  return (
+    <Container>
+      <Content>
+        <Form>
+          <Text style={globalStyles.titulo}>Cantidad</Text>
+          <Grid>
+            <Col>
+              <Button props dark style={{height: 80, justifyContent: 'center', width: '100%'}}
+                onPress={disminuirUno }
+              >
+                <Icon style={{fontSize: 40}} name="remove" />
+              </Button>
+            </Col>
+            <Col>
+              <Input 
+              value={cantidad.toString()} 
+              style={{textAlign: 'center', fontSize: 20}}
+              keyboardType="numeric"
+              onChangeText={(cantidad) => calcularCantidad(cantidad) }
+              />
+            </Col>
+            <Col>
+              <Button props dark style={{height: 80, justifyContent: 'center',width: '100%'}}
+               onPress={incremetarUno}
+              >
+                <Icon style={{fontSize: 40}} name="add" />
+              </Button>
+            </Col>
+          </Grid>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
+
+export default FormularioPlatillo;
+
+const styles = StyleSheet.create({});
