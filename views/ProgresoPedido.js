49f5f67d3ced5c4bd21754e0908e2da8316ea8d8
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-12-09 23:59:05 -0600

    get time for the database

--- views/ProgresoPedido.js
+++ views/ProgresoPedido.js
@@ -3,14 +3,31 @@ import {View, StyleSheet} from 'react-native';
 import {Container, Text, H1, H3, Button} from 'native-base';
 import globalStyles from '../styles/global';
 import {useNavigation} from '@react-navigation/native';
+//Context
 import PedidoContext from '../context/pedidos/pedidosContext';
+//Firebase
+import firebase from '../firebase';
 
 const ProgesoPedido = () => {
+    //context
     const {idPedido} = useContext(PedidoContext);
+    //state
+    const [tiempo, setTiempo] = useState(0);
+
+    useEffect(() => {
+        const obtenerProducto = () => {
+            firebase.db.collection('ordenes').doc(idPedido).onSnapshot(handleSnapshot);
+        }
+        obtenerProducto();
+    },[])
+
+    const handleSnapshot = (doc) => {
+        setTiempo(doc.data().tiempoentrega);
+    }
 
     return (
         <View>
-            <Text>{idPedido}</Text>
+            <Text>{tiempo}</Text>
         </View>
     )
 }
